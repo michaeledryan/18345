4e29171e64799565c55f83fb3dcbc776b327329d
Michael Nye <mnye@andrew.cmu.edu>
2013-01-21 23:02:51 -0500

    Created packet parsing class, doesn't respond to client yet

---
+++ pr1/src/packet/RequestPacket.java
@@ -0,0 +1,45 @@
+package packet;
+
+import java.util.*;
+import java.io.*;
+
+public class RequestPacket {
+	private String request;
+	private Map<String, String> headers = new HashMap<String, String>();
+
+	public RequestPacket(BufferedReader packet) throws IOException {
+		// First parse header line to get request
+		System.out.println("Now parsing packet...");
+		String line = packet.readLine();
+		System.out.println(line);
+		
+		// Get request from middle field
+		if(line == null)
+			throw new IOException();
+		String[] tokens = line.split(" ");
+		if(tokens.length < 3)
+			throw new IOException();
+		request = tokens[1];
+		if(request.equals("/"))
+			request = "/index.html";
+
+
+		// Then loop through header and add to map
+		while (!(line = packet.readLine()).equals("")) {
+			System.out.format("%s\n", line);
+			tokens = line.split(":");
+			if (tokens.length >= 2)
+				headers.put(tokens[0], tokens[1].trim());
+		}
+
+		System.out.println("Packet parsed.");
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public String getHeader(String key) {
+		return headers.get(key);
+	}
+}
--- pr1/src/server/ClientHandler.java
+++ pr1/src/server/ClientHandler.java
@@ -3,6 +3,8 @@ package server;
 import java.io.*;
 import java.net.*;
 
+import packet.*;
+
 public class ClientHandler implements Runnable {
 
 	private static int clientCount = 0;
@@ -17,12 +19,32 @@ public class ClientHandler implements Runnable {
 		client = incoming;
 		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		out = new PrintWriter(client.getOutputStream(), true);
+		out.println("hello");
 	}
 
 	@Override
 	public void run() {
 		System.out.format("Connected to client #%d\n", id);
-		out.format("You are client #%d\n", id);
+		// out.format("You are client #%d\n", id);
+
+		RequestPacket request;
+		try {
+			request = new RequestPacket(in);
+		} catch (IOException e) {
+			System.out.format("Failed to read request packet for client %d.\n",
+					id);
+		}
+		
+		out.println("HTTP/1.1 404 NOT FOUND\r\n\r\n");
+		out.flush();
+		System.out.format("Goodbye client %d\n", id);
+		try {
+			in.close();
+			out.close();
+			client.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
