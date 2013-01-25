f9b7756fa849133894b322236cde94fecbee6adb
Michael Nye <mnye@andrew.cmu.edu>
2013-01-25 03:39:15 -0500

    Cleaned up build.xml, added port number as an optional argument to executable

--- pr1/src/edu/cmu/ece/Vodserver.java
+++ pr1/src/edu/cmu/ece/Vodserver.java
@@ -5,7 +5,21 @@ import server.HTTPServer;
 public class Vodserver {
 
 	public static void main(String[] args) {
-		HTTPServer server = new HTTPServer(18345);
+		int port = 18345;
+		if (args.length > 0) {
+			if (args.length > 1) {
+				System.out.println("Discarding extra args.");
+			}
+
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Port must be an integer");
+				System.exit(1);
+			}
+		}
+
+		HTTPServer server = new HTTPServer(port);
 		server.run();
 	}
 
--- pr1/src/server/HTTPServer.java
+++ pr1/src/server/HTTPServer.java
@@ -1,7 +1,8 @@
 package server;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashSet;
 import java.util.Set;
 
